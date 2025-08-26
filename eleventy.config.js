566f223477eeae76503837308c06a8b8a3626e90
Zach Leatherman <zachleat@users.noreply.github.com>
2025-08-26 14:56:20 -0500

    Fix conflict

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,7 @@
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
+import pluginWebc from "@11ty/eleventy-plugin-webc";
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
-module.exports = function(eleventyConfig) {
+export default function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebc);
 
 	eleventyConfig.ignores.add("README.md");
